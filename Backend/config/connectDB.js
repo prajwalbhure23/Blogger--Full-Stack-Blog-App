2b9577fa3d5bf71d65e3d4dbb82a8f4af588eb46
Prajwal Bhure <137801738+prajwalbhure23@users.noreply.github.com>
2024-03-05 17:30:01 +0530

    Update connectDB.js

--- Backend/config/connectDB.js
+++ Backend/config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Connected to MONGODB at ${mongoose.connection.host}`)
+        console.log(`mongodb+srv://prajwalbhure23:cSWLPLpUTZWc9sy5@cluster0.g8bnbd5.mongodb.net/blogger`)
     } catch (error) {
         console.log(`Mongo connection error: ${error}`)
     }
